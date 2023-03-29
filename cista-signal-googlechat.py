d8ac2111325f2c6a96ca9a69ac84014b27529af1
AIKAWA Shigechika <shige@aikawa.jp>
2023-03-29 11:51:13 +0900

    snip PGP SIGNATURE

--- cista-signal-googlechat.py
+++ cista-signal-googlechat.py
@@ -21,6 +21,7 @@ import datetime
 import os
 import logging
 import argparse
+import re
 
 
 class cista_signal:
@@ -200,6 +201,8 @@ class cista_signal:
             text = (
                 msg["body"].replace("\\r", "").replace("\\n", "\n").replace("\\t", "\t")
             )
+            text = re.sub(r"-----BEGIN PGP SIGNATURE-----([\r\n\S]*)-----END PGP SIGNATURE-----", "-----PGP署名省略-----", text, re.MULTILINE)
+
             if msg["created_at"] == msg["updated_at"]:
                 chat_text = f"*{subject}*\n_公開日時：{msg['created_at']}_\n\n{text}"
             else:
